3b2aefc4071d39edeaad7b62658562561492c750
Hamza el Haydary <hamzaelhaydary@gmail.com>
2025-05-25 22:29:32 +0100

    Add 1-multi_languages.js

--- 0x12/1-multi_languages.js
+++ 0x12/1-multi_languages.js
@@ -1,3 +1,8 @@
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+const myVar1 = "C is fun";
+const myVar2 = "Python is cool";
+const myVar3 = "JavaScript is amazing";
+
+console.log(myVar1);
+console.log(myVar2);
+console.log(myVar3);
+
